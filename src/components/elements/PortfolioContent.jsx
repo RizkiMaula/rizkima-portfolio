d606cb96e310975496d63315d8b200938ed77d16
RizkiMaula <Rmaulana12321@gmail.com>
2025-01-17 09:09:36 +0700

    add some portfolio

--- src/components/elements/PortfolioContent.jsx
+++ src/components/elements/PortfolioContent.jsx
@@ -1,5 +1,7 @@
 import Card from '../fragments/Card';
 import port1 from '../../assets/gempor.jpg';
+import port2 from '../../assets/foodie.jpg';
+import port3 from '../../assets/soto.jpg';
 
 const PortfolioContent = () => {
   return (
@@ -10,15 +12,42 @@ const PortfolioContent = () => {
           directLink="https://gempor-travel.vercel.app/"
           img={port1}
           title="Gempor Travel Website"
-          description="This site is a travel website. Made by React.js, Tailwindcss, Material Tailwind Framework and with API from DibimbingAPI. The site is responsive and has a dark mode. The site is also responsive and has a dark mode."
+          description="This site is a travel website. Made by React.js, Tailwindcss, Material Tailwind Framework and with API from DibimbingAPI. The site is responsive and has a dark mode. This site is My Final Project for the Front-End Web Developer Bootcamp at Dibimbing.id and get score 80."
+          stacks={
+            <div className="flex gap-2">
+              <div className="badge badge-neutral">React.js</div>
+              <div className="badge badge-neutral">TailwindCSS</div>
+              <div className="badge badge-neutral">Axios</div>
+              <div className="badge badge-neutral">React Redux</div>
+            </div>
+          }
         />
         <Card
-          directLink="https://github.com/rizkimaula"
-          img="https://robocuts.com/wp-content/uploads/2012/01/blog-im-1100x500.jpg"
+          directLink="https://foodie-doodie.vercel.app/"
+          img={port2}
+          title="Foodie Doodie"
+          description="This site is a food list from DibimbingAPI. This site is one of my assignments for the Front-End Web Developer Bootcamp at Dibimbing.id. The site made by Next.js and it has two ways rendering, Client Side Rendering (CSR) and Server Side Rendering (SSR) for comparison."
+          stacks={
+            <div className="flex gap-2">
+              <div className="badge badge-neutral">Next.js</div>
+              <div className="badge badge-neutral">TailwindCSS</div>
+              <div className="badge badge-neutral">Axios</div>
+            </div>
+          }
         />
         <Card
-          directLink="https://github.com/rizkimaula"
-          img="https://robocuts.com/wp-content/uploads/2012/01/blog-im-1100x500.jpg"
+          directLink="https://bu-karti-soto-ayam.vercel.app/"
+          img={port3}
+          title="Soto Ayam Bu Karti"
+          description="This site is a restaurant website. This site is one of my assignments for the Front-End Web Developer Bootcamp at Dibimbing.id. This site is my first project using TailwindCSS."
+          stacks={
+            <div className="flex gap-2">
+              <div className="badge badge-neutral">HTML</div>
+              <div className="badge badge-neutral">CSS</div>
+              <div className="badge badge-neutral">TailwindCSS</div>
+              <div className="badge badge-neutral">Javascript</div>
+            </div>
+          }
         />
         <Card
           directLink="https://github.com/rizkimaula"
--- src/components/fragments/Card.jsx
+++ src/components/fragments/Card.jsx
@@ -1,7 +1,14 @@
 import { Link } from 'react-router';
 
 // eslint-disable-next-line react/prop-types
-const Card = ({ img = 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp', title = 'Shoes!', description = 'If a dog chews shoes whose shoes does he choose?', directLink = '/hello' }) => {
+const Card = ({
+  img = 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp',
+  title = 'Shoes!',
+  description = 'If a dog chews shoes whose shoes does he choose?',
+  directLink = '/hello',
+  stacks = <></>,
+  linkText = 'Visit Site',
+}) => {
   return (
     <div className="shadow-xl card bg-base-100 hover:shadow-2xl ">
       <figure>
@@ -13,13 +20,14 @@ const Card = ({ img = 'https://img.daisyui.com/images/stock/photo-1606107557195-
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         <p>{description}</p>
+        {stacks}
         <div className="justify-end card-actions">
           <Link
             to={directLink}
             target="_blank"
             className="btn btn-primary"
           >
-            Visit Page
+            {linkText}
           </Link>
         </div>
       </div>
