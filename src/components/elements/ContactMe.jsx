0013ace5cf22cae75b126ae538e86eb999331d05
RizkiMaula <Rmaulana12321@gmail.com>
2025-01-20 12:32:47 +0700

    hapus border

--- src/components/elements/ContactMe.jsx
+++ src/components/elements/ContactMe.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router';
 const ContactMe = () => {
   return (
     // <div className="min-h-screen bg-base-200">
-    <div className="flex flex-col items-center justify-center border-2 border-black lg:flex-row min-h-screen bg-base-200">
-      <div className="flex flex-col gap-5 p-4 border-2 border-red-500 shadow rounded-2xl bg-base-200 text-tertiary">
+    <div className="flex flex-col items-center justify-center lg:flex-row min-h-screen bg-base-200">
+      <div className="flex flex-col gap-5 p-4 shadow rounded-2xl bg-base-200 text-tertiary">
         <h1 className="text-3xl font-bold">REACH ME OUT</h1>
         <p>Jakarta, Indonesia</p>
         <h1>
@@ -29,7 +29,7 @@ const ContactMe = () => {
           </Link>
         </div>
       </div>
-      <div className="flex flex-col gap-5 p-4 border-2 border-blue-500 shadow rounded-2xl bg-base-200">
+      <div className="flex flex-col gap-5 p-4 shadow rounded-2xl bg-base-200">
         <div className="flex flex-col gap-4 md:flex-row">
           {' '}
           <label className="flex items-center gap-2 input input-bordered">
